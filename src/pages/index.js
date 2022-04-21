d4bb4dc65caa866ee9085a3a920a4331135cb235
Oleg <olsard7@gmail.com>
2022-04-21 14:27:43 +0300

    basic routing example

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as About } from "./About";
+export { default as NoMatch } from "./NoMatch";
+export { default as Home } from "./Home";
+export { default as Dashboard } from "./Dashboard";
